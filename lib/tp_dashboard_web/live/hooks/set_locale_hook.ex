73bceb8b2c7e48ebcc5e09d1ccef87bc853ec86a
Simon <simon@7lieues.io>
2025-03-06 15:25:20 +0100

    chore: removes debug logs

--- lib/tp_dashboard_web/live/hooks/set_locale_hook.ex
+++ lib/tp_dashboard_web/live/hooks/set_locale_hook.ex
@@ -1,7 +1,6 @@
 defmodule TpDashboardWeb.SetLocaleHook do
   def on_mount(:default, %{"lang" => language} = _params, _session, socket)
       when is_binary(language) do
-    IO.puts("We are inside hook")
     Gettext.put_locale(TpDashboardWeb.Gettext, language)
     {:cont, socket}
   end
