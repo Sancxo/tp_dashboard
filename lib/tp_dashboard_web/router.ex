a9c029206e3d53d01edf018c73152e1f87900aab
Simon <simon@7lieues.io>
2025-03-06 10:06:05 +0100

    feat: adds user and dashboard routes to router

--- lib/tp_dashboard_web/router.ex
+++ lib/tp_dashboard_web/router.ex
@@ -18,6 +18,17 @@ defmodule TpDashboardWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    # UserLive
+    live "/users", UserLive.Index, :index
+    live "/users/new", UserLive.Index, :new
+    live "/users/:id/edit", UserLive.Index, :edit
+
+    live "/users/:id", UserLive.Show, :show
+    live "/users/:id/show/edit", UserLive.Show, :edit
+
+    # DashboardLive
+    live "/dashboard/:user_id", DashboardLive.Index
   end
 
   # Other scopes may use custom stacks.
